885e6694a0abf0bb4c013f5b498e010d81bec55b
Juan Bido <juanbido9090@gmail.com>
2020-10-28 00:05:25 -0400

    new pc parts

---
+++ questions.js
@@ -0,0 +1,30 @@
+var questions = [
+    {
+        title: "How old am I?",
+        choices: ["21", "23", "65", "69"],
+        answer: "23"
+    },
+    {
+        title: "Who is my favorite rapper",
+        choices: ["T.I", "Kendrick", "Lil Wayne", "Kanye"],
+        answer: "Kanye"
+    },
+    {
+        title: "Favroite fast food place?",
+        choices: ["Mc Donalds", "Taco Bell", "Wedndy's", "Pizza huy"],
+        answer: "Taco Bell"
+    },
+    {
+        title: "My favorite movie is?",
+        choices: ["Up", "Megamind", "Scotpligrim", "Batman begins"],
+        answer: "Up"
+    },
+    {
+        title: "My favorite cartoon is?",
+        choices: ["Pokemon", "Digimon", "Powerpuff girls", "Avatar"],
+        answer: "Avatar"
+    },
+
+
+];
+console.log(questions);
